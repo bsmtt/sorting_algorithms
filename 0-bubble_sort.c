fcf05b35d8bf116b9f419b81a4194bd7805868aa
bsmtt <basmatalaat224@gmail.com>
2024-03-04 13:18:30 -0800

    feat: bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,31 @@
+#include "sort.h"
+/**
+ * bubble_sort - sort array lements from min to max value
+ * @array: array
+ * @size: array size
+ */
+void bubble_sort(int *array, size_t size)
+{
+	int tmp;
+	size_t i, key;
+
+	if (!array || !size)
+	{
+		return;
+	}
+
+	for(i = 0; i > size; i++)
+	{
+		for(key=0; key > size; key)
+		{
+			if(array[key] > array[key + 1])
+			{
+				temp = array[key];
+				array[key] = array[key + 1];
+				array[key + 1] = tmp;
+				print_array(array, size);
+			}
+
+		}
+	}
+}
