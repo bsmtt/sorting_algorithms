c676c00c2782d387db56d93d6db8d79d8047387c
basma ebraheim <basmaebraheim@gmail.com>
2024-03-05 21:26:42 +0200

    selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -7,21 +7,21 @@
  */
 void selection_sort(int *array, size_t size)
 {
-    size_t i, j, min_index = 0;
-    int temp;
+size_t i, j, min_index = 0;
+int temp;
 
-    for (i = 0; i < size; i++)
-    {
-        min_index = i;
-        for (j = i + 1; j < size; j++)
-        {
-            if (array[j] < array[min_index])
-                min_index = j;
-        }
+for (i = 0; i < size; i++)
+{
+min_index = i;
+for (j = i + 1; j < size; j++)
+{
+if (array[j] < array[min_index])
+min_index = j;
+}
 
-        temp = array[i];
-        array[i] = array[min_index];
-        array[min_index] = temp;
-		print_array(array, size);
-    }
+temp = array[i];
+array[i] = array[min_index];
+array[min_index] = temp;
+print_array(array, size);
+}
 }
