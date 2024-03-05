00934f627767f264c3f907551600171b02268a1a
basmaebraheim <basmaebraheim@gmail.com>
2024-03-05 11:25:54 -0800

    selection sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,27 @@
+#include "sort.h"
+/**
+ * selection_sort - function that sorts an array of integers
+ * in ascending order using the Selection sort algorithm
+ * @array: an array of integers
+ * @size: size of array
+ */
+void selection_sort(int *array, size_t size)
+{
+    size_t i, j, min_index = 0;
+    int temp;
+
+    for (i = 0; i < size; i++)
+    {
+        min_index = i;
+        for (j = i + 1; j < size; j++)
+        {
+            if (array[j] < array[min_index])
+                min_index = j;
+        }
+
+        temp = array[i];
+        array[i] = array[min_index];
+        array[min_index] = temp;
+		print_array(array, size);
+    }
+}
